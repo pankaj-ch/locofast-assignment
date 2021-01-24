4b6116b4e42722776baa4e5fd65ac8a2730ed541
Pankaj Chauhan <Pankaj3960x@gmail.com>
2021-01-25 00:37:45 +0530

    Add dependency react-select and default state for from added

--- src/components/ProductFormDialog/hocProductFormDialog.js
+++ src/components/ProductFormDialog/hocProductFormDialog.js
@@ -1,10 +1,23 @@
 import React, { PureComponent } from "react";
+import Api from "@locofast/api";
 
 const HocProductFormDialog = (WrappedComponent) => {
 	class HocProductForm extends PureComponent {
 		constructor(props) {
 			super(props);
-			this.state = {};
+			this.state = {
+				quantity: "",
+				factory: null,
+				designs: null,
+				factories: [],
+				designs: []
+			};
+		}
+
+		componentDidMount() {
+			Promise.all([Api.getFactories(), Api.getDesigns()])
+				.then(([factories, designs]) => this.setState({ factories, designs }))
+				.catch(console.warn);
 		}
 
 		render() {
